fca0b054dcfeda143dec2da22f6125107ae068a6
aviram07 <avirammahalik818@gmail.com>
2025-04-27 18:27:22 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -17,7 +17,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     rm = InlineKeyboardMarkup(
         [[
-            InlineKeyboardButton("✨ Update Channel", url="https://t.me/+gIP1ETiYnlhkOGU1")
+            InlineKeyboardButton("✨ Update Channel", url="https://t.me/movie_star65")
         ]] 
     )
     await client.send_message(
